78cca1e99e42f4238232ad45163e8aa1b9b679d1
Pavel Tyulnev <pash_tyul95@mail.ru>
2025-04-26 18:48:43 +0200

    Update

--- main/main.ino
+++ main/main.ino
@@ -15,10 +15,10 @@ const char* serverURL = "home.it-horizon.de";
 DHT dht(DHTPIN, DHTTYPE);
 
 // Staubsensor
-int messPin = A0;
-int abtastZeit = 280;
-int verzogerungZeit = 40;
-int schlafZeit = 9680;
+const int messPin = A0;
+const unsigned int abtastZeit_us = 280;
+const unsigned int verzogerungZeit_us = 40;
+const unsigned int schlafZeit_us = 9680;
 
 // CO2 Sensor (MH-Z19)
 #define RX_PIN 12
@@ -28,8 +28,8 @@ MHZ19 myMHZ19;
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
 
 // Reset Button und LED
-#define RESET_BUTTON_PIN 0 // Beispiel: GPIO0
-#define LED_PIN 2 // Beispiel: GPIO2 (eingebaute LED auf vielen Boards)
+#define RESET_BUTTON_PIN 0
+#define LED_PIN 2
 
 WiFiManager wifiManager;
 bool portalActive = false;
@@ -44,15 +44,14 @@ void initSensors() {
 }
 
 float readDustVoltage() {
-  delayMicroseconds(abtastZeit);
+  delayMicroseconds(abtastZeit_us);
   float messwert = analogRead(messPin);
-  delayMicroseconds(verzogerungZeit);
-  delayMicroseconds(schlafZeit);
+  delayMicroseconds(verzogerungZeit_us + schlafZeit_us);
   return messwert * (3.3 / 1024.0);
 }
 
 float calculateDustDensity(float voltage) {
-  return 170 * voltage - 0.1;
+  return max(0.0, 170.0 * voltage - 0.1);
 }
 
 void sendSensorData(float humidity, float temperature, float dustVoltage, float dustDensity, int co2, int co2Temp) {
@@ -71,16 +70,11 @@ void sendSensorData(float humidity, float temperature, float dustVoltage, float 
 
     client.print("GET " + url + " HTTP/1.1\r\n");
     client.print("Host: " + String(serverURL) + "\r\n");
-    client.print("Connection: close\r\n");
-    client.print("\r\n");
-
-    while (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
+    client.print("Connection: close\r\n\r\n");
   } else {
     Serial.println("Verbindung fehlgeschlagen");
   }
+  client.stop();
 }
 
 void setup() {
@@ -96,7 +90,6 @@ void setup() {
     delay(1000);
   }
 
-  // Jetzt versuchen, automatisch zu verbinden:
   if (!wifiManager.autoConnect("HomeStats")) {
     Serial.println("Verbindung fehlgeschlagen! Starte neu...");
     delay(3000);
@@ -110,7 +103,6 @@ void setup() {
   portalActive = false;
   initSensors();
 }
-
 
 void handleButtonAndPortal() {
   if (digitalRead(RESET_BUTTON_PIN) == LOW) {
@@ -141,19 +133,26 @@ void loop() {
 
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+  
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Fehler beim Lesen des DHT-Sensors!");
+    delay(2000);
+    return;
+  }
+
   float dustVoltage = readDustVoltage();
   float dustDensity = calculateDustDensity(dustVoltage);
   int co2 = myMHZ19.getCO2();
   int co2Temp = myMHZ19.getTemperature();
 
   Serial.println("--- Messwerte ---");
-  Serial.print("Temperatur: "); Serial.println(temperature);
-  Serial.print("Feuchtigkeit: "); Serial.println(humidity);
-  Serial.print("Staubspannung: "); Serial.println(dustVoltage);
-  Serial.print("Staubdichte: "); Serial.println(dustDensity);
-  Serial.print("CO2: "); Serial.println(co2);
-  Serial.print("CO2 Temp: "); Serial.println(co2Temp);
+  Serial.printf("Temperatur: %.1f C\n", temperature);
+  Serial.printf("Feuchtigkeit: %.1f %%\n", humidity);
+  Serial.printf("Staubspannung: %.3f V\n", dustVoltage);
+  Serial.printf("Staubdichte: %.2f ug/m3\n", dustDensity);
+  Serial.printf("CO2: %d ppm\n", co2);
+  Serial.printf("CO2 Temp: %d C\n", co2Temp);
 
   sendSensorData(humidity, temperature, dustVoltage, dustDensity, co2, co2Temp);
-  delay(10000);
+  delay(2000);
 }
